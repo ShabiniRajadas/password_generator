ea64a0c38dc110b01ced369719866729c78aa8e6
ShabiniRajadas <shabiniraja90@gmail.com>
2024-01-06 15:51:35 -0800

    random password generator's commit -2

--- lib/random_password_generator/railtie.rb
+++ lib/random_password_generator/railtie.rb
@@ -1,9 +1,9 @@
 # lib/random_password_generator/railtie.rb
 module RandomPasswordGenerator
-    class Railtie < ::Rails::Railtie
-			ActiveSupport.on_load(:action_view) do
-				include RandomPasswordGenerator::ViewHelper
-			end
+  class Railtie < ::Rails::Railtie
+    ActiveSupport.on_load(:action_view) do
+      include RandomPasswordGenerator::ViewHelper
     end
+  end
 end
   
